28e33bd9ed3a71c584effd1fb33c6bfa1528c94e
Alan Xiao <alanxiao2013@gmail.com>
2020-09-06 07:52:22 +0800

    remove jar

--- src/main/java/com/axcdevelopment/odin/Core/MessageEventListeners/ModeratorEventListener.java
+++ src/main/java/com/axcdevelopment/odin/Core/MessageEventListeners/ModeratorEventListener.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class ModeratorEventListener extends MemberEventListener {
+public class ModeratorEventListener extends OdinMessageEventListener {
 
     @Override
     public void onEvent(@NotNull GenericEvent event) {
